cdf89cb84c0469313e12bf5a287489e2ef00bea4
ihufa <42546508+ihufa@users.noreply.github.com>
2018-11-10 01:09:58 +0100

    Add files via upload

---
+++ test1/togglemenu.js
@@ -0,0 +1,6 @@
+   
+    let headerElem = document.getElementById('header');
+    let menu = document.getElementById('menu');
+    menu.onclick = function() {
+      headerElem.classList.toggle('open');
+ };
